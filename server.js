68695316e1aa46a4a3bdf08b285fd09c032b4d61
amalianurf <amalia19006@mail.unpad.ac.id>
2024-04-08 20:23:40 +0700

    added authorization middleware

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const middleware = require('./middleware/authorization');
+
 const userController = require('./controllers/users');
 const sectorController = require('./controllers/sectors');
 const companyController = require('./controllers/companies');
@@ -22,20 +24,26 @@ app.prepare().then(() => {
     // sector api
     server.get('/api/sectors', sectorController.getAll);
     server.get('/api/sector/:id', sectorController.getById);
-    server.post('/api/sector', sectorController.create);
-    server.put('/api/sector/:id', sectorController.update);
-    server.delete('/api/sector/:id', sectorController.delete);
 
     // company api
     server.get('/api/companies', companyController.getAll);
     server.get('/api/company/:id', companyController.getById);
-    server.post('/api/company', companyController.create);
-    server.put('/api/company/:id', companyController.update);
-    server.delete('/api/company/:id', companyController.delete);
 
     // opportunity api
     server.get('/api/opportunities', opportunityController.getAll);
     server.get('/api/opportunity/:id', opportunityController.getById);
+
+    // admin only
+    server.use(middleware.authMiddleware);
+
+    server.post('/api/company', companyController.create);
+    server.put('/api/company/:id', companyController.update);
+    server.delete('/api/company/:id', companyController.delete);
+
+    server.post('/api/sector', sectorController.create);
+    server.put('/api/sector/:id', sectorController.update);
+    server.delete('/api/sector/:id', sectorController.delete);
+
     server.post('/api/opportunity', opportunityController.create);
     server.put('/api/opportunity/:id', opportunityController.update);
     server.delete('/api/opportunity/:id', opportunityController.delete);
