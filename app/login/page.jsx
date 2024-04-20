8a6cc34e2aa1a7a149cb7ac04aedad509efe0f1b
amalianurf <amalia19006@mail.unpad.ac.id>
2024-04-20 23:40:02 +0700

    fixed login form style

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -23,6 +23,8 @@ function page() {
     const handleLogin = async (e) => {
         e.preventDefault()
 
+        toast.loading('Login diproses...')
+
         if (form.email == '' || form.password == '') {
             toast.error('Harap lengkapi semua data.')
         } else {
@@ -40,9 +42,11 @@ function page() {
                 }
                 return response.json()
             }).then((data) => {
+                toast.dismiss()
                 Cookies.set('access-token', data.accessToken)
                 router.push('/admin/dashboard')
             }).catch((error) => {
+                toast.dismiss()
                 toast.error(error.message)
                 console.error('Error:', error.message)
             })
@@ -58,17 +62,17 @@ function page() {
                     <form onSubmit={handleLogin} className='flex flex-col gap-9 w-[440px]'>
                         <div className='flex flex-col gap-1'>
                             <label htmlFor='email' className='font-medium'>Email</label>
-                            <input type='email' name='email' value={form.email} onChange={handleFormChange} placeholder='Email' className='w-full px-4 py-2 border border-grey rounded-lg text-base outline-none' required />
+                            <input type='email' name='email' value={form.email} onChange={handleFormChange} placeholder='Email' className='w-full px-4 py-2 border border-grey rounded-lg text-base outline-none focus:border-iris/[.7] focus:ring-1 ring-iris/[.6]' required />
                         </div>
                         <div className='flex flex-col gap-1'>
                             <label htmlFor='password' className='font-medium'>Password</label>
                             <div className='relative'>
-                                <input type={showPassword ? 'text' : 'password'} name='password' value={form.password} onChange={handleFormChange} placeholder='Password' className='w-full px-4 py-2 border border-grey rounded-lg text-base outline-none' required />
+                                <input type={showPassword ? 'text' : 'password'} name='password' value={form.password} onChange={handleFormChange} placeholder='Password' className='w-full px-4 py-2 border border-grey rounded-lg text-base outline-none focus:border-iris/[.7] focus:ring-1 ring-iris/[.6]' required />
                                 <Button type={'button'} onClick={() => setShowPassword(!showPassword)} name={showPassword ? <VisibilityOffOutlinedIcon /> : <VisibilityOutlinedIcon />} buttonStyle={'absolute right-4 -top-1/5 translate-y-1/3 text-grey'} />
                             </div>
                         </div>
                         <div className='flex justify-center'>
-                            <Button type={'submit'} name={'MASUK'} buttonStyle={'w-1/2 py-3 text-white font-bold bg-iris rounded-lg'} />
+                            <Button type={'submit'} name={'MASUK'} buttonStyle={'w-1/2 py-3 text-white font-bold bg-iris hover:bg-iris/[.3] rounded-lg'} />
                         </div>
                     </form>
                 </section>
