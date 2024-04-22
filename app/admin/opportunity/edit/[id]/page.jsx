2651d2d3b3a2dbe0ff6d58dc5f03d2b1603cdb17
amalianurf <amalia19006@mail.unpad.ac.id>
2024-04-22 22:17:23 +0700

    added admin table, add, and edit opportunity pages

---
+++ app/admin/opportunity/edit/[id]/page.jsx
@@ -0,0 +1,141 @@
+'use client'
+import React, { useState, useEffect } from 'react'
+import { useParams, useRouter } from 'next/navigation'
+import OpportunityForm from '@component/components/form/OpportunityForm'
+import Button from '@component/components/Button'
+import toast from 'react-hot-toast'
+import Cookies from 'js-cookie'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+
+function page() {
+    const [opportunity, setOpportunity] = useState({
+        name: '',
+        activity_type: '',
+        duration: '',
+        description: '',
+        quota: '',
+        start_period: '',
+        min_semester: '',
+        salary: '',
+        company_id: ''
+    })
+    const [selectedOption, setSelectedOption] = useState({
+        company: null,
+        activity_type: null,
+    })
+    const [loading, setLoading] = useState()
+    const { id } = useParams('id')
+    const router = useRouter()
+
+    useEffect(() => {
+        const fetchDataOpportunity = async () => {
+            await fetch(`${process.env.NEXT_PUBLIC_SERVER}/api/opportunity/${id}`).then(async (response) => {
+                if (!response.ok) {
+                    return response.json().then(error => {
+                        throw new Error(error.message)
+                    })
+                }
+                return response.json()
+            }).then( async(opportunity) => {
+                const period = new Date(opportunity.start_period)
+                setOpportunity({
+                    name: opportunity.name,
+                    activity_type: opportunity.activity_type,
+                    duration: opportunity.duration,
+                    description: opportunity.description,
+                    quota: opportunity.quota,
+                    start_period: period.toISOString().split('T')[0],
+                    min_semester: opportunity.min_semester,
+                    salary: opportunity.salary,
+                    company_id: opportunity.company_id
+                })
+                await fetch(`${process.env.NEXT_PUBLIC_SERVER}/api/company/${opportunity.company_id}`).then(async (response) => {
+                    if (!response.ok) {
+                        return response.json().then(error => {
+                            throw new Error(error.message)
+                        })
+                    }
+                    return response.json()
+                }).then((company) => {
+                    setSelectedOption({
+                        company: {
+                            name: 'company_id',
+                            value: company.id,
+                            label: company.brand_name
+                        },
+                        activity_type: {
+                            name: 'activity_type',
+                            value: opportunity.activity_type,
+                            label: opportunity.activity_type
+                        }
+                    })
+                }).catch((error) => {
+                    console.error('Error:', error)
+                    setLoading(false)
+                })
+                setLoading(false)
+            }).catch((error) => {
+                console.error('Error:', error)
+                setLoading(false)
+            })
+        }
+
+        fetchDataOpportunity()
+    }, [])
+
+    const handleInputChange = (e) => {
+        setOpportunity({ ...opportunity, [e.target.name]: e.target.value })
+    }
+
+    const handleSelectChange = (option, name) => {
+        setSelectedOption({ ...selectedOption, [name]: option })
+        if (option) {
+            setOpportunity({ ...opportunity, [option.name]: option.value })
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        toast.loading('Mengirim data...')
+
+        if (opportunity.name == '' || opportunity.description == '' || opportunity.start_period == '') {
+            toast.error('Harap lengkapi nama, deskripsi, dan periode mulai magang.')
+        } else {
+            fetch(`${process.env.NEXT_PUBLIC_SERVER}/api/opportunity/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Authorization': Cookies.get('access-token'),
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(opportunity)
+            }).then(async (response) => {
+                if (!response.ok) {
+                    return response.json().then(error => {
+                        throw new Error(error.message)
+                    })
+                }
+                return response.json()
+            }).then((data) => {
+                toast.dismiss()
+                toast.success(data.message)
+            }).catch((error) => {
+                toast.dismiss()
+                toast.error(error.message)
+                console.error('Error:', error.message)
+            })
+        }
+    }
+
+    return (
+        <section className='flex flex-col gap-6 p-10'>
+            <div className='flex items-center gap-2 text-iris'>
+                <Button type={'button'} onClick={() => router.back()} name={<ArrowBackIcon fontSize='large' />} buttonStyle={'flex items-center p-1.5 rounded-full hover:bg-grey-light/[.3]'} />
+                <h2>Edit Data Magang</h2>
+            </div>
+            <OpportunityForm value={opportunity} selectedOption={selectedOption} buttonName={'Ubah Data'} handleInputChange={handleInputChange} handleSelectChange={handleSelectChange} handleSubmit={handleSubmit} />
+        </section>
+    )
+}
+
+export default page
---
+++ app/admin/opportunity/page.jsx
@@ -0,0 +1,177 @@
+'use client'
+import React, { useState, useEffect } from 'react'
+import Button from '@component/components/Button'
+import { DataGrid, useGridApiContext, useGridSelector, gridPageSelector, gridPageCountSelector, GridToolbarQuickFilter } from '@mui/x-data-grid'
+import { Pagination, PaginationItem, createTheme, ThemeProvider } from '@mui/material'
+import Cookies from 'js-cookie'
+import toast from 'react-hot-toast'
+
+function page() {
+    const [opportunities, setOpportunities] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [filterModel, setFilterModel] = useState({
+        items: [],
+        quickFilterExcludeHiddenColumns: false,
+    })
+
+    useEffect(() => {
+        const fetchDataOpportunities = async () => {
+            await fetch(`${process.env.NEXT_PUBLIC_SERVER}/api/opportunities`).then(async (response) => {
+                if (!response.ok) {
+                    return response.json().then(error => {
+                        throw new Error(error.message)
+                    })
+                }
+                return response.json()
+            }).then((data) => {
+                setOpportunities(data)
+                setLoading(false)
+            }).catch((error) => {
+                console.error('Error:', error)
+                setLoading(false)
+            })
+        }
+
+        fetchDataOpportunities()
+    }, [opportunities])
+
+    const handleDelete = (id) => {
+        toast.loading('Menghapus data...')
+        fetch(`${process.env.NEXT_PUBLIC_SERVER}/api/opportunity/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': Cookies.get('access-token'),
+                'Content-Type': 'application/json'
+            }
+        }).then(async (response) => {
+            if (!response.ok) {
+                return response.json().then(error => {
+                    throw new Error(error.message)
+                })
+            }
+            return response.json()
+        }).then((data) => {
+            toast.dismiss()
+            toast.success(data.message)
+        }).catch((error) => {
+            toast.dismiss()
+            toast.error(error.message)
+            console.error('Error:', error)
+        })
+    }
+
+    const formatDate = (dateString) => {
+        const date = new Date(dateString)
+        const day = date.getDate().toString().padStart(2, '0')
+        const month = (date.getMonth() + 1).toString().padStart(2, '0')
+        const year = date.getFullYear()
+        return `${day}-${month}-${year}`
+    }
+
+    const formatRowData = (data) => {
+        return data.map((row, index) => {
+            return { ...row, no: index + 1, duration: `${row.duration.toString()} bulan`, start_period: formatDate(row.start_period) }
+        })
+    }
+
+    const rows = formatRowData(opportunities)
+
+    const columns = [
+        { field: 'no', headerName: 'No.', width: 70 },
+        { field: 'name', headerName: 'Nama', flex: 1 },
+        { field: 'activity_type', headerName: 'Sistem', flex: 1 },
+        { field: 'duration', headerName: 'Durasi', flex: 1 },
+        { field: 'start_period', headerName: 'Periode Mulai', flex: 1 },
+        { field: 'quota', headerName: 'Kuota', flex: 1 },
+        { field: 'min_semester', headerName: 'Min Semester', flex: 1 },
+        {
+            field: 'actions',
+            headerName: 'Aksi',
+            width: 150,
+            renderCell: (params) => (
+                <div className='flex justify-center items-center gap-2 w-full h-full'>
+                    <Button type={'button'} href={`/admin/opportunity/edit/${params.id}`} name={'Edit'} buttonStyle={'text-center font-medium text-sm text-white bg-green hover:bg-green/[.3] rounded-md px-2 py-1 w-full'} />
+                    <Button type={'button'} onClick={() => handleDelete(params.id)} name={'Hapus'} buttonStyle={'text-center font-medium text-sm text-white bg-red hover:bg-red/[.3] rounded-md px-2 py-1 w-full'} />
+                </div>
+            )
+        }
+    ]
+
+    const paginationTheme = createTheme({
+        palette: {
+            iris: {
+                main: '#5D5FEF',
+                contrastText: '#FFFFFF'
+            },
+        },
+    })
+
+    const customPagination = () => {
+        const apiRef = useGridApiContext();
+        const page = useGridSelector(apiRef, gridPageSelector)
+        const pageCount = useGridSelector(apiRef, gridPageCountSelector)
+
+        return (
+            <Pagination
+                color='iris'
+                count={pageCount}
+                page={page + 1}
+                renderItem={(props2) => <PaginationItem {...props2} disableRipple />}
+                onChange={(event, value) => apiRef.current.setPage(value - 1)}
+            />
+        )
+    }
+
+    return (
+        <section className='p-10'>
+            {loading ? (
+                <div>Loading...</div>
+            ) : (
+                <>
+                    <div className='relative'>
+                        <Button type={'button'} href={'/admin/opportunity/add'} name={'Tambah Data'} buttonStyle={'absolute right-0 top-0 z-10 w-fit h-fit text-center font-medium text-white text-base bg-iris hover:bg-iris/[.3] rounded-lg px-4 py-2'} />
+                        <ThemeProvider theme={paginationTheme}>
+                            <DataGrid
+                                autoHeight
+                                pagination
+                                rows={rows}
+                                columns={columns}
+                                slots={{
+                                    toolbar: GridToolbarQuickFilter,
+                                    pagination: customPagination
+                                }}
+                                slotProps={{
+                                    toolbar: {
+                                        showQuickFilter: true
+                                    }
+                                }}
+                                filterModel={filterModel}
+                                onFilterModelChange={(newModel) => setFilterModel(newModel)}
+                                initialState={{pagination: {paginationModel: {pageSize: 100}}}}
+                                classes={{
+                                    columnHeader: 'bg-neutral-dark !font-semibold',
+                                    row: 'odd:bg-white even:bg-neutral',
+                                }}
+                                sx={{
+                                    border: 0,
+                                    borderRadius: 2,
+                                    '& .MuiDataGrid-main': { borderRadius: 2, boxShadow: '0 2px 8px #0000001F', marginTop: '12px' },
+                                    '& .MuiDataGrid-footerContainer': { border: 0, marginTop: '12px' },
+                                    '& .MuiDataGrid-cell': { border: 0 },
+                                    '& .MuiDataGrid-topContainer::after': { height: 0 },
+                                    '& .MuiFormControl-root': { border: 1, borderRadius: 2, borderColor: '#7C7C7C', width: '312px', padding: '2px 10px' },
+                                    '& .MuiFormControl-root:focus-within': { boxShadow: 'inset 0 0 0 1px #5D5FEF', borderColor: '#5D5FEF' },
+                                    '& .MuiSvgIcon-root': { color: '#7C7C7C' },
+                                    '& .MuiInputBase-root::before': { borderBottom: '0 !important' },
+                                    '& .MuiInputBase-root::after': { borderBottom: 0 }
+                                }}
+                            />
+                        </ThemeProvider>
+                    </div>
+                </>
+            )}
+        </section>
+    )
+}
+
+export default page
