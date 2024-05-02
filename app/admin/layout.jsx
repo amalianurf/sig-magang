d6cec6ed6e1e51725590db70931d12cc0ea46572
amalianurf <amalia19006@mail.unpad.ac.id>
2024-05-02 20:13:14 +0700

    fixed admin layout

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,8 +1,9 @@
 'use client'
-import { usePathname } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import SideBar from '@component/components/navigations/SideBar'
 import Button from '@component/components/Button'
 import { Toaster } from 'react-hot-toast'
+import Cookies from 'js-cookie'
 
 function layout({ children }) {
     const path = usePathname()
@@ -12,6 +13,14 @@ function layout({ children }) {
         '/admin/opportunity': 'Data Magang',
         '/admin/company': 'Data Perusahaan',
         '/admin/sector': 'Data Sektor'
+    }
+    const router = useRouter()
+    const token = Cookies.get('access-token')
+
+    if (typeof window !== 'undefined') {
+        if (!token) {
+            router.push('/login')
+        }
     }
 
     const getTitle = (path) => {
@@ -23,17 +32,22 @@ function layout({ children }) {
         return null
     }
 
+    const logout = () => {
+        Cookies.remove('access-token')
+        router.push('/login')
+    }
+
     return (
         <>
             <Toaster position='top-center' reverseOrder={false} />
             <div className='flex bg-black'>
                 <SideBar />
-                <div className='flex flex-col w-full min-h-screen ml-64'>
-                    <header className='w-full flex justify-between px-10 py-4 text-white'>
+                <div className='flex flex-col w-full min-h-screen'>
+                    <header className='max-w-full ml-64 flex justify-between px-10 py-4 text-white'>
                         <h3>{getTitle(path)}</h3>
-                        <Button type={'button'} name={'Logout'} buttonStyle={'bg-red px-4 py-2 text-white font-bold text-sm uppercase rounded-lg'} />
+                        <Button type={'button'} onClick={logout} name={'Logout'} buttonStyle={'bg-red px-4 py-2 text-white font-bold text-sm uppercase rounded-lg'} />
                     </header>
-                    <main className='w-full h-full bg-white rounded-t-2xl'>
+                    <main className='max-w-full ml-64 h-full bg-white rounded-t-2xl'>
                         {children}
                     </main>
                 </div>
