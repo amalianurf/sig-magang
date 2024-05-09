83db7462e013312656646e05255f493a9bcb53b9
amalianurf <amalia19006@mail.unpad.ac.id>
2024-05-09 20:01:46 +0700

    added filter opportunities by sector

--- app/page.jsx
+++ app/page.jsx
@@ -8,9 +8,15 @@ const Map = dynamic(() => import('@component/components/map/Map'), { ssr: false 
 function page() {
     const [navbarHeight, setNavbarHeight] = useState()
     const [geoJsonData, setGeoJsonData] = useState()
+    const [filteredGeoJsonData, setFilteredGeoJsonData] = useState()
     const [sectors, setSectors] = useState()
     const [companies, setCompanies] = useState()
-    const [opportunities, setOpportunities] = useState()
+    const [filteredCompanies, setFilteredCompanies] = useState()
+    const [selectedSector, setSelectedSector] = useState()
+    const [dateRange, setDateRange] = useState({
+        start: '',
+        end: ''
+    })
     const [loading, setLoading] = useState({
         geojson: true,
         sector: true,
@@ -28,6 +34,7 @@ function page() {
                 return response.json()
             }).then((data) => {
                 setGeoJsonData(data)
+                setFilteredGeoJsonData(data)
                 setLoading({ ...loading, geojson: false })
             }).catch((error) => {
                 console.error('Error:', error)
@@ -45,6 +52,7 @@ function page() {
                 return response.json()
             }).then((data) => {
                 setCompanies(data)
+                setFilteredCompanies(data)
                 setLoading({ ...loading, company: false })
             }).catch((error) => {
                 console.error('Error:', error)
@@ -74,17 +82,109 @@ function page() {
         fetchDataSectors()
     }, [])
 
+    const handleDateChange = (e) => {
+        setDateRange({ ...dateRange, [e.target.name]: e.target.value })
+    }
+
+    const handleSelectChange = (option) => {
+        setSelectedSector(option)
+
+        if (option) {
+            fetch(`${process.env.NEXT_PUBLIC_SERVER}/api/opportunities?sector=${option.value}`).then(async (response) => {
+                if (!response.ok) {
+                    return response.json().then(error => {
+                        throw new Error(error.message)
+                    })
+                }
+                return response.json()
+            }).then((data) => {
+                if (data) {
+                    const groupedOpportunityByCity = data.reduce((accumulator, item) => {
+                        if (!accumulator[item.Company.city]) {
+                            accumulator[item.Company.city] = []
+                        }
+        
+                        accumulator[item.Company.city].push(item)
+        
+                        return accumulator
+                    }, {})
+        
+                    const uniqueCompanyIds = [...new Set(data.map(item => item.company_id))]
+        
+                    const uniqueCompanies = uniqueCompanyIds.map(id => {
+                        const company = data.find(item => item.company_id === id)
+        
+                        return {
+                            id: id,
+                            city: company.Company.city,
+                            location: company.Company.location
+                        }
+                    })
+        
+                    setFilteredCompanies(uniqueCompanies)
+        
+                    const groupedCompanyByCity = uniqueCompanies.reduce((accumulator, item) => {
+                        if (!accumulator[item.city]) {
+                            accumulator[item.city] = []
+                        }
+        
+                        accumulator[item.city].push(item)
+        
+                        return accumulator
+                    }, {})
+        
+                    const newGeoJsonFeatures = geoJsonData.features.map(feature => {
+                        const city = feature.properties.city
+        
+                        if (groupedOpportunityByCity[city] && groupedOpportunityByCity[city].length > 0) {
+                            return {
+                                ...feature,
+                                properties: {
+                                    ...feature.properties,
+                                    companies: groupedCompanyByCity[city].length,
+                                    opportunities: groupedOpportunityByCity[city].length
+                                }
+                            }
+                        } else {
+                            return null
+                        }
+                    }).filter(feature => feature !== null)
+
+                    if (newGeoJsonFeatures.length > 0) {
+                        setFilteredGeoJsonData({
+                            type: 'FeatureCollection',
+                            features: newGeoJsonFeatures
+                        })
+                    } else {
+                        setFilteredGeoJsonData(null)
+                    }
+                }
+            }).catch((error) => {
+                console.error('Error:', error)
+            })
+        } else {
+            setFilteredCompanies(companies)
+            setFilteredGeoJsonData(geoJsonData)
+        }
+    }
+
     return (
         <>
             <header>
                 <NavBar setNavbarHeight={setNavbarHeight} />
             </header>
             <main style={{ paddingTop: navbarHeight }} className='w-full'>
-                {loading.geojson && loading.sector && loading.company ? (
-                    <div className='p-10'>Loading...</div>
-                ) : (
-                    <Map geoJsonData={geoJsonData} navbarHeight={navbarHeight} companies={companies} />
-                )}
+                <Map
+                    navbarHeight={navbarHeight}
+                    loading={loading}
+                    geoJsonData={filteredGeoJsonData}
+                    companies={filteredCompanies}
+                    sectors={sectors}
+                    dateRange={dateRange}
+                    selectedSector={selectedSector}
+                    handleDateChange={handleDateChange}
+                    handleSelectChange={handleSelectChange}
+                />
             </main>
         </>
     )
--- components/map/Map.jsx
+++ components/map/Map.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { MapContainer, TileLayer, GeoJSON, Marker, Popup } from 'react-leaflet'
 import MarkerClusterGroup from 'react-leaflet-cluster'
 import 'leaflet/dist/leaflet.css'
 import 'leaflet-defaulticon-compatibility'
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'
+import Select from 'react-select'
+import Button from '@component/components/Button'
+import CircleIcon from '@mui/icons-material/Circle'
 
 function Map(props) {
+    const [mapKey, setMapKey] = useState(0)
+
+    useEffect(() => {
+        setMapKey((prevKey) => prevKey + 1)
+    }, [props.geoJsonData])
+
     // perhitungan simpangan baku
     const standardDeviation = () => {
         const data = props.geoJsonData.features
@@ -31,8 +40,8 @@ function Map(props) {
 
     // perhitungan batas atas dan batas bawah
     const UpperLowerBounds = (sd) => {
-        const upperBounds = sd.mean + (0.5 * sd.standardDeviation)
-        const lowerBounds = sd.mean - (0.5 * sd.standardDeviation)
+        const upperBounds = sd.mean + (0.3 * sd.standardDeviation)
+        const lowerBounds = sd.mean - (0.3 * sd.standardDeviation)
 
         return { upperBounds, lowerBounds }
     }
@@ -50,34 +59,89 @@ function Map(props) {
             : 'grey'
     }
 
+    const selectStyle = {
+        control: (styles, { isFocused }) => ({ ...styles, width: 'fit-content', fontSize: '16px', padding: '6px 12px', border: isFocused ? '1px solid #5D5FEF99 !important' : '1px solid #7C7C7C !important', borderRadius: '8px', outline: 'none !important', boxShadow: isFocused ? 'inset 0 0 0 1px #5D5FEFB3' : 'none', '&.hover': { border: '1px solid #7C7C7C !important'} }),
+        valueContainer: (styles) => ({ ...styles, padding: '0' }),
+        placeholder: (styles) => ({ ...styles, margin: '0', color: '#9da4b0', fontSize: '16px' }),
+        input: (styles) => ({ ...styles, margin: '0', padding: '0', fontSize: '16px' }),
+        dropdownIndicator: (styles) => ({ ...styles, padding: '0 0 0 4px', color: '#7C7C7C' }),
+        clearIndicator: (styles) => ({ ...styles, padding: '0 0 0 4px', color: '#7C7C7C99' }),
+        indicatorSeparator: (styles) => ({ ...styles, display: 'none' })
+    }
+
     return (
-        <MapContainer center={[-1.3631627162310562, 118.42289645522916]} zoom={5} style={{ height: `calc(100vh - ${props.navbarHeight}px)`, width: '100%' }}>
-            <TileLayer
-                attribution='&copy <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-            />
-            {props.geoJsonData && (
-                <GeoJSON
-                    data={props.geoJsonData}
-                    style={(feature) => ({
-                        fillColor: getPolygonColor(feature.properties.opportunities),
-                        color: '#ffffff',
-                        weight: 1
-                    })}
+        <>
+            {props.sectors && (
+                <div style={{ top: `${props.navbarHeight}px` }} className='fixed right-0 flex items-center gap-3 px-7 py-5 rounded-bl-lg bg-white/[.6] z-[1000]'>
+                    <Select
+                        name='sector_id'
+                        onChange={(selectedOption) => props.handleSelectChange(selectedOption)}
+                        value={props.selectedSector}
+                        placeholder={'Pilih Sektor'}
+                        options={props.sectors.map(data => ({
+                            name: 'sector_id',
+                            value: data.id,
+                            label: data.name
+                        }))}
+                        isSearchable={true}
+                        isClearable={true}
+                        styles={selectStyle}
+                    />
+                    <form onSubmit={'/'} className='flex items-center gap-3'>
+                        <div className='flex items-center gap-1'>
+                            <input type='date' name='start_date' value={props.dateRange.start} onChange={props.handleDateChange} className='relative w-fit px-3 py-1.5 border border-grey rounded-lg text-base outline-none focus:border-iris/[.7] focus:ring-1 ring-iris/[.6]' required />
+                            <div>to</div>
+                            <input type='date' name='end_date' value={props.dateRange.end} onChange={props.handleDateChange} className='relative w-fit px-3 py-1.5 border border-grey rounded-lg text-base outline-none focus:border-iris/[.7] focus:ring-1 ring-iris/[.6]' required />
+                        </div>
+                        <Button type={'submit'} name={'Filter Data'} buttonStyle={'w-fit px-3 py-2 text-white font-bold bg-iris hover:bg-iris/[.3] rounded-lg'} />
+                    </form>
+                </div>
+            )}
+            <MapContainer center={[-1.3631627162310562, 118.42289645522916]} zoom={5} style={{ height: `calc(100vh - ${props.navbarHeight}px)`, width: '100%' }}>
+                <TileLayer
+                    attribution='&copy <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
                 />
+                {props.geoJsonData && (
+                    <GeoJSON
+                        key={mapKey}
+                        data={props.geoJsonData}
+                        style={(feature) => ({
+                            fillColor: getPolygonColor(feature.properties.opportunities),
+                            color: '#ffffff',
+                            weight: 1
+                        })}
+                    />
+                )}
+                {props.companies && (
+                    <MarkerClusterGroup chunkedLoading >
+                        {props.companies.map((company, index) => (
+                            <Marker key={index} position={[company.location.coordinates[0], company.location.coordinates[1]]} title={company.brand_name}>
+                                <Popup>
+                                    {company.brand_name}
+                                </Popup>
+                            </Marker>
+                        ))}
+                    </MarkerClusterGroup>
+                )}
+            </MapContainer>
+            {props.geoJsonData && (
+                <div className='fixed right-0 bottom-0 w-full px-6 py-2 flex justify-end items-center gap-10 bg-white z-[1000]'>
+                    <div className='flex items-center gap-1'>
+                        <CircleIcon fontSize='small' className='text-green' />
+                        <div>Lowongan &gt; {UpperLowerBounds(standardDeviation()).upperBounds.toFixed(2)}</div>
+                    </div>
+                    <div className='flex items-center gap-1'>
+                        <CircleIcon fontSize='small' className='text-yellow' />
+                        <div>{UpperLowerBounds(standardDeviation()).upperBounds.toFixed(2)} &gt;= Lowongan &gt;= {UpperLowerBounds(standardDeviation()).lowerBounds.toFixed(2)}</div>
+                    </div>
+                    <div className='flex items-center gap-1'>
+                        <CircleIcon fontSize='small' className='text-red' />
+                        <div>Lowongan &lt; {UpperLowerBounds(standardDeviation()).lowerBounds.toFixed(2)}</div>
+                    </div>
+                </div>
             )}
-            {props.companies && (
-                <MarkerClusterGroup chunkedLoading >
-                    {props.companies.map((company, index) => (
-                        <Marker key={index} position={[company.location.coordinates[0], company.location.coordinates[1]]} title={company.brand_name}>
-                            <Popup>
-                                {company.brand_name}
-                            </Popup>
-                        </Marker>
-                    ))}
-                </MarkerClusterGroup>
-            )}
-        </MapContainer>
+        </>
     )
 }
 
