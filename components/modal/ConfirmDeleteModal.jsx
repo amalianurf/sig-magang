b2e3baf1b21906a8dde09bd136940b077d271d11
amalianurf <amalia19006@mail.unpad.ac.id>
2024-06-20 19:34:26 +0700

    Merge updated files from 'main' into production

--- components/modal/ConfirmDeleteModal.jsx
+++ components/modal/ConfirmDeleteModal.jsx
@@ -13,8 +13,8 @@ function ConfirmDeleteModal(props) {
                 <h3>Konfirmasi Hapus</h3>
                 <div>Apakah Anda yakin ingin menghapus data?</div>
                 <div className='flex justify-end items-center gap-5'>
-                    <Button type={'button'} onClick={handleCloseModal} name={'Batal'} buttonStyle={'w-fit px-4 py-2 text-iris font-bold bg-white ring-1 ring-iris hover:ring-iris/[.3] hover:text-iris/[.3] rounded-lg'} />
-                    <Button type={'button'} onClick={() => props.handleDelete(props.confirmDelete.id)} name={'Hapus'} buttonStyle={'w-fit px-4 py-2 text-white font-bold bg-red hover:bg-red/[.3] rounded-lg'} />
+                    <Button type={'button'} onClick={handleCloseModal} name={'Tidak'} buttonStyle={'w-fit px-4 py-2 text-iris font-bold bg-white ring-1 ring-iris hover:ring-iris/[.3] hover:text-iris/[.3] rounded-lg'} />
+                    <Button type={'button'} onClick={() => props.handleDelete(props.confirmDelete.id)} name={'Yakin'} buttonStyle={'w-fit px-4 py-2 text-white font-bold bg-red hover:bg-red/[.3] rounded-lg'} />
                 </div>
                 <Button type={'button'} onClick={handleCloseModal} name={<CloseIcon />} buttonStyle={'absolute top-3 right-3 flex items-center p-1.5 rounded-full text-grey hover:bg-grey-light/[.3]'} />
             </div>
